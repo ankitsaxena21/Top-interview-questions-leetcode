1c4393e77c218edc54eb15d923cceb52e434b40d
Ankit Saxena <47791661+ankitsaxena21@users.noreply.github.com>
2020-05-10 17:27:38 +0530

    Create LL5.py

---
+++ LL5.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        res = []
+        while head:
+            res.append(head.val)
+            head = head.next
+        return res == res[::-1]
+        
